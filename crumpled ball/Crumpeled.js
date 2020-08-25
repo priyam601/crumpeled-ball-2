9aa6d7509a85e8ac2637adb7e2312c59f847f363
priyam601 <68382598+priyam601@users.noreply.github.com>
2020-08-25 19:45:26 +0530

    Add files via upload

---
+++ crumpled ball/Crumpeled.js
@@ -0,0 +1,46 @@
+class Crumpeled{
+    
+      constructor(x,y,r){
+		  
+		var options={
+			isStatic:false,
+			restitution:0.3,
+			friction:0.5,
+			density:1.2
+		}
+
+		this.x=x;
+		this.y=y;
+		this.r=r
+
+		this.body=Bodies.circle(this.x,this.y,this.r/2,options)
+		this.sling1 = loadImage("sprites/paper.png");
+
+		World.add(world,this.body);
+
+	}
+	display(){
+			
+		var paperpos=this.body.position;		
+
+		push()
+
+		translate(paperpos.x, paperpos.y);
+
+		rectMode(CENTER)
+
+		image(this.sling1,15,30);
+
+		strokeWeight(3);
+		fill(255,0,255)
+		
+		ellipse(0,0,this.r, this.r);
+		pop()
+			
+	}
+
+	}
+	
+	
+
+		
